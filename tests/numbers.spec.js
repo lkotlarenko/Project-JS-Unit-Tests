c33d6b0ae75457dbf96ecec36834e09dc5ccb19f
lkotlarenko <lkotlarenko@protonmail.com>
2021-11-16 11:43:37 -0300

    Added tests to function "numbers"

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -16,10 +16,13 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
-    fail('Teste vazio!');
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    expect(numbers([1, 2, 3, 4, 5])).toEqual(true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    expect(numbers([1, 2, '3', 4, 5])).toEqual(false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    expect(numbers([1, 'a', 3])).toEqual(false);
     // Escreva um teste em que a função recebe [' '] e retorna false
+    expect(numbers([' '])).toEqual(false);
   });
 });
