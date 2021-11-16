6f020514f95bbd294fc2269007d75b48be4f80ca
lkotlarenko <lkotlarenko@protonmail.com>
2021-11-16 12:02:23 -0300

    Function "vqv"

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, yold) => {
+  if (name && yold) {
+    return `Oi, meu nome é ${name}!
+Tenho ${yold} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  }
+  return undefined;
+};
 
 module.exports = vqv;
